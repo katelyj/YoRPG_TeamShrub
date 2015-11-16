2e5cb147e554c4f8832187f7a2a2c3590dffd818
Kate Ly <kjohnston@stuy.edu>
2015-11-16 18:59:23 -0500

    Create Rogue.java

---
+++ Rogue.java
@@ -0,0 +1,33 @@
+/*=============================================
+  class Rogue -- protagonist of Ye Olde RPG
+  =============================================*/
+
+public class Rogue extends Character {
+
+    /*=============================================
+      default constructor
+      pre:  instance vars are declared
+      post: initializes instance vars.
+      =============================================*/
+    public Rogue() {
+	_hitPts = 125;
+	_strength = 100;
+	_defense = 40;
+	_attack = .4;
+    }
+
+    /*=============================================
+      overloaded constructor
+      pre:  instance vars are declared
+      post: initializes instance vars. _name is set to input String.
+      =============================================*/
+    public Rogue( String name ) {
+	this();
+	_name = name;
+    }
+
+    public String about() {
+	return "Rouges: Rogues are master of dark, they are those who walks alone. Dexterity and agility allow rouges to stealthily avoid enemies by picking locks and stealing items of importance. Rogues would much rather stab their enemies in the back than face them head on. Though they are unable to withstand large amounts of damage, rogues are extraordinarily good at dodging attacks and quickly dealing with their enemies through trickery and poisons.";
+    }
+
+}//end class Rogue
